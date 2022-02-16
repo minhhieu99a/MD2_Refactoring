7eacf63f38931dee491c73a910a1b4fbed8b5a02
minhhieu99a <hieunb99@gmail.com>
2022-02-16 19:49:40 +0700

    tennisgame

---
+++ src/TennisGame.java
@@ -0,0 +1,60 @@
+public class TennisGame {
+    public static String getScore( int checkPoint, int scoreofPlayer2) {
+        String score = "";
+        int tempScore=0;
+        if (checkPoint==scoreofPlayer2)
+        {
+            switch (checkPoint)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+        }
+        else if (checkPoint>=4 || scoreofPlayer2>=4)
+        {
+            int minusResult = checkPoint-scoreofPlayer2;
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
+            else score ="Win for player2";
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = checkPoint;
+                else { score+="-"; tempScore = scoreofPlayer2;}
+                switch(tempScore)
+                {
+                    case 0:
+                        score+="Love";
+                        break;
+                    case 1:
+                        score+="Fifteen";
+                        break;
+                    case 2:
+                        score+="Thirty";
+                        break;
+                    case 3:
+                        score+="Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
+}
